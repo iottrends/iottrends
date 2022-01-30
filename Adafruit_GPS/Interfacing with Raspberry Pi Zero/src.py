85149489e724e15063bdfe6b7b12148a08a8a05a
Gautham Shankar <81378777+GauthamShankar21@users.noreply.github.com>
2022-01-30 11:55:41 +0530

    Source Code

---
+++ Adafruit_GPS/Interfacing with Raspberry Pi Zero/src.py
@@ -0,0 +1,27 @@
+import time
+import board
+import busio
+import adafruit_gps
+
+RX = board.10
+TX = board.8
+uart = busio.UART(TX, RX, baudrate=115200, timeout=30)
+gps = adafruit_gps.GPS(uart, debug=False)
+
+gps.send_command(b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+gps.send_command(b'PMTK220,1000')
+
+last_print = time.monotonic()
+while True:
+
+    gps.update()
+
+    current = time.monotonic()
+    if current - last_print >= 1.0:
+        last_print = current
+        if not gps.has_fix:
+            print('Waiting for fix...')
+            continue
+        print('=' * 40)  # Print a separator line.
+        print('Latitude: {0:.6f} degrees'.format(gps.latitude))
+        print('Longitude: {0:.6f} degrees'.format(gps.longitude))

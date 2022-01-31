33947869c3f2e54afd4674b98bafd5e5a1e6ddb1
Gautham Shankar <81378777+GauthamShankar21@users.noreply.github.com>
2022-01-31 22:58:10 +0530

    Source_Code

---
+++ SeeedStudio Grove GPS/Interfacing_With_ESP8266/src.c
@@ -0,0 +1,32 @@
+#include <arduino.h>
+#include <SoftwareSerial.h>
+SoftwareSerial SoftSerial(20, 19);
+unsigned char buffer[64]; 
+int count=0; 
+void setup()
+{
+ SoftSerial.begin(9600); 
+ Serial.begin(9600); 
+}
+void loop()
+{
+ if (SoftSerial.available()) 
+ {
+ while(SoftSerial.available()) 
+ {
+ buffer[count++]=SoftSerial.read(); 
+ if(count == 64)break;
+ }
+5
+ Serial.write(buffer,count);
+ clearBufferArray(); 
+ count = 0; 
+ }
+ if (Serial.available())
+SoftSerial.write(Serial.read()); 
+}
+void clearBufferArray() 
+{
+ for (int i=0; i<count;i++)
+ { buffer[i]=NULL;} 
+}

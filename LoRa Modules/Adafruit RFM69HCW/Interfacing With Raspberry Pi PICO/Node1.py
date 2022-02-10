5d7c7ada87b9a3f582b974b84eb655afa15d6234
Gautham Shankar <81378777+GauthamShankar21@users.noreply.github.com>
2022-02-10 07:30:16 +0530

    Source code for Node1

---
+++ LoRa Modules/Adafruit RFM69HCW/Interfacing With Raspberry Pi PICO/Node1.py
@@ -0,0 +1,46 @@
+import time
+import board
+import busio
+import digitalio
+import adafruit_rfm9x
+from machine import *
+
+
+transmit_interval = 10
+
+
+RADIO_FREQ_MHZ = 915.0  # Frequency of the radio in Mhz. Must match your
+# module! Can be a value like 915.0, 433.0, etc.
+
+CS = digitalio.DigitalInOut(board.GP9)
+RESET = digitalio.DigitalInOut(board.GP22)
+
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
+
+rfm9x.enable_crc = True
+rfm9x.node = 1
+rfm9x.destination = 2
+counter = 0
+rfm9x.send(
+    bytes("Startup message {} from node {}".format(counter, rfm9x.node), "UTF-8")
+)
+
+print("Waiting for packets...")
+now = time.monotonic()
+while True:
+    packet = rfm9x.receive(with_header=True)
+    if packet is not None:
+        print("Received (raw header):", [hex(x) for x in packet[0:4]])
+        print("Received (raw payload): {0}".format(packet[4:]))
+        print("Received RSSI: {0}".format(rfm9x.last_rssi))
+    if time.monotonic() - now > transmit_interval:
+        now = time.monotonic()
+        counter = counter + 1
+        rfm9x.send(
+            bytes(
+                "message number {} from node {}".format(counter, rfm9x.node), "UTF-8"
+            ),
+            keep_listening=True,
+        )
+

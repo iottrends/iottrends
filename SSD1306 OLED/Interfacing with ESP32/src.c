de1f0f8df76d17a08a9c3eea9de047c3d18ce371
Gautham Shankar <81378777+GauthamShankar21@users.noreply.github.com>
2022-02-17 16:09:25 +0530

    Source Code

---
+++ SSD1306 OLED/Interfacing with ESP32/src.c
@@ -0,0 +1,22 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128 
+#define SCREEN_HEIGHT 64 
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+void setup() {
+  Serial.begin(115200);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  display.println("Hello, world!");
+  display.display(); 
+}
+void loop() {  
+}

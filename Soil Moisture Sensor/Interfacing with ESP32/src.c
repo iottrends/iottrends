414fb34f44af97b4899a28ff1d04c307157400e4
Gautham Shankar <81378777+GauthamShankar21@users.noreply.github.com>
2022-03-06 16:43:04 +0530

    Source Code

---
+++ Soil Moisture Sensor/Interfacing with ESP32/src.c
@@ -0,0 +1,18 @@
+int moisturePin = 13;
+int thresholdValue = 800;
+
+void setup(){
+  pinMode(rainPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int sensorValue = analogRead(moisturePin);
+  Serial.print(sensorValue);
+  if(sensorValue < thresholdValue){
+    Serial.println(" - Doesn't need watering");
+  }
+  else {
+    Serial.println(" - Time to water your plant");
+  }
+  delay(500);

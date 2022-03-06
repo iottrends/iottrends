a5f2a83f3e43f3ae85fff044117786aa01d249d5
Gautham Shankar <81378777+GauthamShankar21@users.noreply.github.com>
2022-03-06 11:43:31 +0530

    Source Code

---
+++ BME280/Interfacing with Arduino/src.c
@@ -0,0 +1,56 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME280 bme; 
+
+unsigned long delayTime;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("BME280 test"));
+
+  bool status;
+  
+  status = bme.begin();  
+  if (!status) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+  
+  Serial.println("-- Default Test --");
+  delayTime = 1000;
+
+  Serial.println();
+}
+
+
+void loop() { 
+  printValues();
+  delay(delayTime);
+}
+
+
+void printValues() {
+  Serial.print("Temperature = ");
+  Serial.print(bme.readTemperature());
+  Serial.println(" *C");
+  
+  
+  Serial.print("Pressure = ");
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.println(" hPa");
+  
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
+  
+  Serial.print("Humidity = ");
+  Serial.print(bme.readHumidity());
+  Serial.println(" %");
+  
+  Serial.println();
+}

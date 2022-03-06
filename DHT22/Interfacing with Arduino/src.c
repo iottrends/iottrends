99735a5fe76ced5640affd89b3e5e3010a7d9e20
Jagrathi J Nayak <74891994+Jagrathi231843@users.noreply.github.com>
2022-03-06 11:00:32 +0530

    Source Code

---
+++ DHT22/Interfacing with Arduino/src.c
@@ -0,0 +1,44 @@
+#include "DHT.h"
+
+#define DHTPIN 9  
+
+#define DHTTYPE DHT22
+
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600); 
+  Serial.println("DHTxx test!");
+ 
+  dht.begin();
+}
+
+void loop() {
+   delay(2000);
+
+  float h = dht.readHumidity();
+
+  float t = dht.readTemperature();
+
+  float f = dht.readTemperature(true);
+  
+
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  float hi = dht.computeHeatIndex(f, h);
+
+  Serial.print("Humidity: "); 
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: "); 
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *F\t");
+  Serial.print("Heat index: ");
+  Serial.print(hi);
+  Serial.println(" *F");
+}
